3198e4c51bac029358669c53095c372cd05fc761
Tony Torp (TAMK) <tony.torp@tuni.fi>
2025-09-12 14:55:33 +0300

    Add, delete fav cities

--- weather-app/src/screens/FavouriteCitiesScreen.jsx
+++ weather-app/src/screens/FavouriteCitiesScreen.jsx
@@ -1,13 +1,42 @@
+import CitySelector from "../components/CitySelector";
 import Header from "../components/Header";
+import {useState} from "react"
 
-const favCities = [
- "Tampere", "Turku", "Helsinki", "Oulu", "Jyväskylä"
-];
 
 // Miten luon listan/table rowt tms itemit datan pohjalta?
 // Miten käsittelen dynaamisesti dataa
 
 const FavouriteCitiesScreen = () => {
+    const [favCities, setFavCities] = useState( 
+        [
+            { id: 1, cityName: "Tampere", country: "FI"}, 
+            { id: 2, cityName: "Stockholm", country: "SE"}, 
+            { id: 3, cityName: "Helsinki", country: "FI"}, 
+            { id: 4, cityName: "London", country: "UK"},       
+        ]
+    );
+
+    const addNewCity = (newCityName)=>{
+        // Luodaan uusi kaupunki nimeltä newCity
+        const newCity = { id: favCities.length, cityName: newCityName, country: "FI"}
+
+        // Jos korvattaisiin olemassa olevan kaupungin nimi (demo)
+        // const modifiedCity = { ...newCity, cityName: "Uusi nimi"};
+        // console.log(modifiedCity);
+        // otetaan olemassa oleva favCities ja lisätään siihen uusi kaupunki 
+        // Spread -operaattori ...
+        setFavCities( [...favCities, newCity])
+        console.log(newCity)
+    }
+
+    const handleDelete = (id) => {
+        // Mitä tämä vaatii??
+        // Moderni listasta tai tietorakenteesta poisto JS:ssä
+        // käytetään filter -funktiota ja luodaan uusi lista, jossa kaikki muut paitsi filterin löytämä olio
+        // Poistettava jää suodattimen ulkopuolelle ja muut muodostavat uuden listan
+        setFavCities( favCities.filter((city) => city.id !== id));
+    }
+
     return(
         <div>
             <Header title={"FavouriteCities"}></Header>
@@ -21,12 +50,16 @@ const FavouriteCitiesScreen = () => {
                 <tbody>
                     { favCities.map((city, index) =>(
                         <tr key={index}>
-                          <td>{city}</td>
-                          <td><button>Select</button><button>Delete</button></td> 
+                          <td>{city.cityName}</td>
+                          <td>
+                            <button>Select</button>
+                            <button onClick={() => handleDelete(city.id)}>Delete</button>
+                           </td> 
                         </tr>
                     ))}
                 </tbody>
             </table>
+            <CitySelector onChange={addNewCity} ></CitySelector>
             
         </div>
     )
