9346f7640f86cd7b014171efc8ec4bf22fd06697
Tony Torp (TAMK) <tony.torp@tuni.fi>
2025-09-12 11:11:37 +0300

    Use effect, conditional rendering

--- weather-app/src/screens/CurrentWeatherScreen.jsx
+++ weather-app/src/screens/CurrentWeatherScreen.jsx
@@ -1,45 +1,60 @@
 import WeatherDataDisplay from "../components/WeatherDataDisplay"
 import Header from "../components/Header";
 import CitySelector from "../components/CitySelector";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+
 
 const CurrentWeatherScreen = () => {
-    const [city, setCity] = useState("Helsinki")
+    const [city, setCity] = useState("Helsinki");
+    const [fetchingData, setFetchingData] = useState(false)
     const [weatherData, setWeatherData] = useState({
        description: "Sunny",
        temperature: 14,
        windSpeed: 3.4 
     });
 
-    const cityUpdated= ( newCity ) =>{
-        console.log("City vaihdettu ", newCity)
-        setCity( newCity )
-        setWeatherData({
-            description: "Cloudy",
-            temperature: 13,
-            windSpeed: 1 
-        });
+    // [] sisään laitetaan listana riippuvuudet. Jos riippuvuus muuttuu, suoritetaan useEffect. 
+    // Jos [] tyhjä, suoritetaan vain ensimmäisen renderöinnin jälkeen
+    useEffect( () => {
+        console.log("Renderöinti")
         fetchWeatherData();
-    }
+    }, [city]);
 
     const fetchWeatherData = async () => {  // async-await ES6+ syntaksi (korvaa aiemman then - catch jne.)
         // Haetaan säätiedot palvelimelta
         try{
             const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6c433438776b5be4ac86001dc88de74d`;
+            setFetchingData(true)
+            // keinotekoinen viive 2s
+            await new Promise((resolve)=>setTimeout(resolve,1500))
             const response = await fetch(URL);
             const weatherJson = await response.json();
             console.log( weatherJson );
+            setFetchingData(false)
             setWeatherData({
-                description: weatherJson.weather[0].main,
-                temperature: weatherJson.main.temp,
-                windSpeed: weatherJson.wind.speed
+                // ? on ES2020 optional chaining -operaattori, jos ei dataa ole, palautuu undefined (ei poikkeusta)
+                description: weatherJson.weather?.[0]?.main,
+                temperature: weatherJson.main?.temp,
+                windSpeed: weatherJson.wind?.speed
             })
         }
         catch(err){
             console.log(err);
+            // navigator.onLine voi testata, onko selain verkossa (true on, false ei)
         }
     }
 
+    // Todo: Conditional Rendering
+    if( fetchingData ){
+        return(
+            <>
+                <Header title={city}></Header>
+                <br/><br/><br/>
+                <h2>Ladataan säätietoja...</h2>
+            </>
+            
+        )
+    }
     return ( 
         <>  
             <Header title={city}></Header>
@@ -48,7 +63,7 @@ const CurrentWeatherScreen = () => {
                 temperature ={weatherData.temperature} 
                 windSpeed={weatherData.windSpeed}>
             </WeatherDataDisplay>         
-            <CitySelector onChange={cityUpdated}></CitySelector> 
+            <CitySelector onChange={setCity}></CitySelector> 
         </> 
     )
 }
