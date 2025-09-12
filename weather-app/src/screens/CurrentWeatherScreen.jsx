c2c83548398ba5688d26afa329378e3ba96591e9
Tony Torp (TAMK) <tony.torp@tuni.fi>
2025-09-12 10:20:16 +0300

    Weather Fetching

--- weather-app/src/screens/CurrentWeatherScreen.jsx
+++ weather-app/src/screens/CurrentWeatherScreen.jsx
@@ -5,16 +5,50 @@ import {useState} from 'react';
 
 const CurrentWeatherScreen = () => {
     const [city, setCity] = useState("Helsinki")
+    const [weatherData, setWeatherData] = useState({
+       description: "Sunny",
+       temperature: 14,
+       windSpeed: 3.4 
+    });
 
     const cityUpdated= ( newCity ) =>{
         console.log("City vaihdettu ", newCity)
         setCity( newCity )
+        setWeatherData({
+            description: "Cloudy",
+            temperature: 13,
+            windSpeed: 1 
+        });
+        fetchWeatherData();
     }
+
+    const fetchWeatherData = async () => {  // async-await ES6+ syntaksi (korvaa aiemman then - catch jne.)
+        // Haetaan säätiedot palvelimelta
+        try{
+            const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6c433438776b5be4ac86001dc88de74d`;
+            const response = await fetch(URL);
+            const weatherJson = await response.json();
+            console.log( weatherJson );
+            setWeatherData({
+                description: weatherJson.weather[0].main,
+                temperature: weatherJson.main.temp,
+                windSpeed: weatherJson.wind.speed
+            })
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
     return ( 
         <>  
             <Header title={city}></Header>
-            <WeatherDataDisplay description="Sunny" temperature ={14} windSpeed={4.5}></WeatherDataDisplay>
-            <CitySelector onChange={cityUpdated}></CitySelector>     
+            <WeatherDataDisplay 
+                description={weatherData.description} 
+                temperature ={weatherData.temperature} 
+                windSpeed={weatherData.windSpeed}>
+            </WeatherDataDisplay>         
+            <CitySelector onChange={cityUpdated}></CitySelector> 
         </> 
     )
 }
---
+++ weather-app/src/utils.js
@@ -0,0 +1,55 @@
+/*const mockupWeatherResponse = {
+  "coord": {
+    "lon": 22.2687,
+    "lat": 60.4515
+  },
+  "weather": [
+    {
+      "id": 311,
+      "main": "Drizzle",
+      "description": "drizzle rain",
+      "icon": "09d"
+    },
+    {
+      "id": 500,
+      "main": "Rain",
+      "description": "light rain",
+      "icon": "10d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 14.22,
+    "feels_like": 14.18,
+    "temp_min": 14.16,
+    "temp_max": 14.67,
+    "pressure": 1015,
+    "humidity": 95,
+    "sea_level": 1015,
+    "grnd_level": 1012
+  },
+  "visibility": 10000,
+  "wind": {
+    "speed": 5.66,
+    "deg": 150,
+    "gust": 11.32
+  },
+  "rain": {
+    "1h": 0.77
+  },
+  "clouds": {
+    "all": 100
+  },
+  "dt": 1757660618,
+  "sys": {
+    "type": 1,
+    "id": 1364,
+    "country": "FI",
+    "sunrise": 1757649075,
+    "sunset": 1757696609
+  },
+  "timezone": 10800,
+  "id": 633679,
+  "name": "Turku",
+  "cod": 200
+}*/
